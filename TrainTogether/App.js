fd00ccee0b8f0f505cbda6db965c8695b6451692
ryanguai <e0694442@u.nus.edu>
2022-06-12 00:12:18 +0800

    added login to app.js (for testing)

--- TrainTogether/App.js
+++ TrainTogether/App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from "react-native-login-screen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LoginScreen
+  logoImageSource={require("/Users/ryanguai/Documents/Brainhack/TrainTogether/assets/traintogether.png")}
+  onLoginPress={() => {}}
+  onSignupPress={() => {}}
+  onEmailChange={(email: string) => {}}
+  onPasswordChange={(password: string) => {}}
+/>
+
       <StatusBar style="auto" />
     </View>
   );
